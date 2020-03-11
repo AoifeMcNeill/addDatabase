96d6c7919988d73345a5083cea8d5222c2920947
AOIFE MCNEILL (1805874) <1805874@rgu.ac.uk>
2020-03-11 12:20:32 +0000

    fix index.js

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 var http = require("http");
+fs = require('fs');
+const PORT = process.env.PORT || 8080;
 
 http.createServer(function (request, response) {
     //Send the HTTP header
     //HTTP Status: 200: OK
     //Content Type: text/plain
-    response.writeHead(200, {'Content-Type': 'text/plain'});
+    
+    //write to our server. set the configuration for the response
+    response.writeHead(200, {
+        "Content-Type": "text/html",
+        "Access-Control-Allow-Origin": "*"
+    });
+    var readStream = fs.createReadStream(__dirname + "/hate_ball_frontend.html");
 
-    //Send the response body as "Hello World"
-    response.end('Hello World\n');
-}).listen(8080);
+    //send a message
+    readStream.pipe(response);
+    
+}).listen(PORT);
 
 //Console will print the message
 console.log('Server running at http://127.0.0.1:8080/');
